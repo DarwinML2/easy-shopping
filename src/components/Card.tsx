36fb033bf682b1698b8e93fc8f70cf7ac5f5f54d
DarwinML2 <darwinmass.ml2@gmail.com>
2024-07-08 15:50:18 -0400

    created Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+import { CoffeeIcon } from 'lucide-react'
+
+export default function Card() {
+  return (
+    <article>
+      <ul>
+        <li>
+          <h2>Cafe</h2>
+          <CoffeeIcon />
+          <span>$1</span>
+        </li>
+      </ul>
+    </article>
+  )
+}
