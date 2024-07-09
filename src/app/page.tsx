871bdfb90c0fd64f12a0df9a79d263735d9e31a3
DarwinML2 <darwinmass.ml2@gmail.com>
2024-07-08 20:06:37 -0400

    used Card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Card from '@/components/Card'
+
 export default function Home() {
   return (
     <main>
       <h1>Choose your food</h1>
+      <div>
+        <Card />
+      </div>
     </main>
   )
 }
