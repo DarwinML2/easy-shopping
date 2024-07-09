611f0d396a5be9aa4807b3abad802cc38f4452fe
DarwinML2 <darwinmass.ml2@gmail.com>
2024-07-08 20:09:07 -0400

    added h1 style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Card from '@/components/Card'
 export default function Home() {
   return (
     <main>
-      <h1>Choose your food</h1>
+      <h1 className='my-5 text-4xl font-bold text-center'>Choose your food</h1>
       <div>
         <Card />
       </div>
