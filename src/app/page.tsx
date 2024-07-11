154f545c7ad85733f99379692f9ae922c86e7c34
DarwinML2 <darwinmass.ml2@gmail.com>
2024-07-11 12:49:12 -0400

    added paddin bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Card from '@/components/Card'
 
 export default function Home() {
   return (
-    <main>
+    <main className='pb-8'>
       <h1 className='my-5 text-4xl font-bold text-center'>Choose your food</h1>
       <div>
         <Card />
