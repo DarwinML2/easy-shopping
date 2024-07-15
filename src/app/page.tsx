eb9a757d1f704f9f7126e9a6102f0b99fff962cc
DarwinML2 <darwinmass.ml2@gmail.com>
2024-07-15 16:36:49 -0400

    used component OrderList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Card from '@/components/Card'
+import OrderList from '@/components/OrderList'
 
 export default function Home() {
   return (
     <main className='pb-8'>
-      <h1 className='my-5 text-4xl font-bold text-center'>Choose your food</h1>
-      <div>
+      <h1 className='my-5 text-4xl font-bold text-center'>Choose your food.</h1>
+      <div className='flex gap-x-2'>
         <Card />
+        <OrderList />
       </div>
     </main>
   )
